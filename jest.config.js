7adc8a7105cf5598ee45e5d1fa5c22232833b46c
zima <genki.yajima@gmail.com>
2019-10-23 11:33:52 +0900

    Add test

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  "roots": [
+    "<rootDir>"
+  ],
+  "testMatch": ["<rootDir>/__tests__/**/*.test.ts"],
+  "transform": {
+    "^.+\\.tsx?$": "ts-jest"
+  },
+  "moduleFileExtensions": [
+    "ts",
+    "tsx",
+    "js",
+    "jsx",
+    "json",
+    "node"
+  ],
+}
